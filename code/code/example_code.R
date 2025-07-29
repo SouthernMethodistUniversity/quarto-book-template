e4ac29d12b91abf6576cfa4d7ed4521d27cb6bef
mateolangston <mateolangston@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    deploy: a4ec7b4a49289f639c29797e009e9e2942417899

---
+++ code/code/example_code.R
@@ -0,0 +1,13 @@
+##-----------------------##
+## Example Code - R Code ##
+##-----------------------##
+
+# Add example R code files here, if needed
+# Update page that you want this file attached to with:
+
+#---
+#code-links:
+#  - text: example_code.R
+#    icon: file-code
+#    href: code/example_code.R
+#---
